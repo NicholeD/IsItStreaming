7e9890aed9ddcec7dc8f78d0ff2fbed7acfb7397
NicholeD <nicholedavid@gmail.com>
2024-04-03 15:13:23 -0600

    Added Quiz folder and models, added account profile, added user model

---
+++ Application/src/Quiz/Answer.java
@@ -0,0 +1,9 @@
+package Quiz;
+
+public class Answer {
+    private String answer;
+    public Answer(String answer) { this.answer = answer;}
+
+    public String getAnswer(){return this.answer;}
+    public void setAnswer(String answer){this.answer = answer;}
+}
---
+++ Application/src/Quiz/Question.java
@@ -0,0 +1,10 @@
+package Quiz;
+
+public class Question {
+    private String question;
+
+    public Question(String question) {this.question = question;}
+
+    public String getQuestion(){return this.question;}
+    public void setQuestion(String question){}
+}
